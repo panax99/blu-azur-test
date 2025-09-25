b3f97c850f9361e1f955a3c73a6ea87b9ff0851b
Pana <panarandri@gmail.com>
2025-09-25 15:19:49 +0300

    feat: fix layout of global App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import './App.css'
+import Footer from './components/layout/Footer/Footer'
+import FifthSection from './features/home/sections/FifthSection/FifthSection'
+import FirstSection from './features/home/sections/FirstSection/FirstSection'
+import FourthSection from './features/home/sections/FourthSection/FourthSection'
+import HeroSection from './features/home/sections/HeroSection/HeroSection'
+import SecondSection from './features/home/sections/SecondSection/SecondSection'
+import SixthSection from './features/home/sections/SixthSection/SixthSection'
+import ThirdSection from './features/home/sections/ThirdSection/ThirdSection'
+
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className='font-mulish overflow-x-hidden'>
+      <HeroSection/>
+      <FirstSection/>
+      <SecondSection/>
+      <ThirdSection/>
+      <FourthSection/>
+      <FifthSection/>
+      <SixthSection/>
+      <Footer/>
+    </div>
   )
 }
 
